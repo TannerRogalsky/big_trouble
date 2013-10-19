6f107f7ad249b611ad7c200907b7ebd824096d1c
Tanner Rogalsky <the.t.dawg@gmail.com>
2013-10-19 11:02:47 -0400

    final win lose screens

--- states/game/game_lose.lua
+++ states/game/game_lose.lua
@@ -1,25 +1,26 @@
 local Lose = Game:addState('Lose')
 
 function Lose:enteredState()
-  self.bg = self.preloaded_image["apep.jpg"]
+  self.bg = self.preloaded_image["romjam_lose.png"]
   self.scalex = g.getWidth() / self.bg:getWidth()
   self.scaley = g.getHeight() / self.bg:getHeight()
 
-  self.text = "Sorry, You Lose"
+  -- self.text = "Sorry, You Lose"
 
-  self.temp_font = g.newFont("fonts/xerxes-regular.ttf", 128)
+  -- self.temp_font = g.newFont("fonts/xerxes-regular.ttf", 128)
 end
 
 function Lose:render()
   g.setColor(COLORS.white:rgb())
   g.draw(self.bg, 0, 0, 0, self.scalex, self.scaley)
 
-  g.setFont(self.temp_font)
-  g.setColor(COLORS.black:rgb())
-  g.printf(self.text, 0, g.getHeight() / 2, g.getWidth(), "center")
-  g.setFont(self.default_font)
+  -- g.setFont(self.temp_font)
+  -- g.setColor(COLORS.black:rgb())
+  -- g.printf(self.text, 0, g.getHeight() / 2, g.getWidth(), "center")
+  -- g.setFont(self.default_font)
 
-  g.printf("Press Enter to try again.", 0, g.getHeight() - 40, g.getWidth(), "center")
+  g.setColor(COLORS.white:rgb())
+  g.printf("Press Enter to try again.", 0, g.getHeight() - 80, g.getWidth(), "center")
 end
 
 function Lose:keyreleased(key, unicode)
--- states/game/game_win.lua
+++ states/game/game_win.lua
@@ -1,24 +1,25 @@
 local Win = Game:addState('Win')
 
 function Win:enteredState()
-  self.bg = self.preloaded_image["barque_of_ra.jpg"]
+  self.bg = self.preloaded_image["romjam_win.png"]
   self.scalex = g.getWidth() / self.bg:getWidth()
   self.scaley = g.getHeight() / self.bg:getHeight()
 
   self.text = "Congratulations! You Win!"
 
-  self.temp_font = g.newFont("fonts/xerxes-regular.ttf", 128)
+  -- self.temp_font = g.newFont("fonts/xerxes-regular.ttf", 128)
 end
 
 function Win:render()
   g.setColor(COLORS.white:rgb())
   g.draw(self.bg, 0, 0, 0, self.scalex, self.scaley)
 
-  g.setFont(self.temp_font)
-  g.setColor(COLORS.black:rgb())
-  g.printf(self.text, 0, g.getHeight() / 2, g.getWidth(), "center")
-  g.setFont(self.default_font)
+  -- g.setFont(self.temp_font)
+  -- g.setColor(COLORS.black:rgb())
+  -- g.printf(self.text, 0, g.getHeight() / 2, g.getWidth(), "center")
+  -- g.setFont(self.default_font)
 
+  g.setColor(COLORS.white:rgb())
   g.printf("Press Enter to try again.", 0, g.getHeight() - 80, g.getWidth(), "center")
 end
 
